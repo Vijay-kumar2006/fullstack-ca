d190cb70c740083e9d0e7f23f1a961273b7891a8
Vijay-kumar2006 <vijaykumar.vk3105@gmail.com>
2025-02-27 10:46:02 +0530

    done

---
+++ team-showcase/src/components.jsx/TeamMemberCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import  './TeamCard.css'
+export default function TeamMemberCard() {
+  return (
+    <div>
+        <div><h2>TeamMembername :John </h2>
+        <p>TeamMember Position : Software Engineer</p>
+        </div>
+        <div><h2>TeamMembername: Jack</h2>
+        <p>TeamMember Position : tester</p>
+        </div>
+        <div><h2>TeamMembername: Jay</h2>
+        <p>TeamMember Position : Backend Developer</p>
+        </div>
+        <div><h2>TeamMembername: Vijay</h2>
+        <p>TeamMember Position : Frontend Developer</p>
+        </div>
+    </div>
+    
+  )
+}
